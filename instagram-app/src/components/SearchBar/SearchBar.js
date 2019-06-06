1e0a7ea54d51a84e9c65c29d9b4bc4e9b13f45e1
Ang Xu <angxu1234@gmail.com>
2019-06-06 17:36:46 -0500

    searchbar refactored

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,6 +1,49 @@
 import React from "react";
-import "./SearchBar.scss";
+// import "./SearchBar.scss";
 import instaTitle from "./InstaTitle.png";
+import styled from "styled-components";
+
+const SearchBarCSS = styled.div`
+	display: flex;
+	justify-content: space-between;
+	padding: 2%;
+`;
+
+const SearchLeft = styled.div`
+	display: flex;
+	font-weight: bold;
+	width: 30%;
+`;
+
+const InstaLogo = styled.div`
+	font-size: 1.5rem;
+	padding-right: 5%;
+`;
+
+const InstaTitle = styled.div`
+	padding: 3% 0 0 3%;
+	border-left: solid grey 1px;
+`;
+
+const InstaImg = styled.img`
+	width: 40%;
+`;
+
+const SearchInput = styled.input`
+	text-align: center;
+	font-family: FontAwesome;
+	width: 20%;
+	font-size: 0.7rem;
+`;
+
+const SearchRight = styled.div`
+	display: flex;
+	width: 30%;
+	justify-content: space-around;
+	padding: 1% 0 0 10%;
+`;
+
+// Refactor Styling
 
 class SearchBar extends React.Component {
 	constructor(props) {
@@ -15,21 +58,20 @@ class SearchBar extends React.Component {
 
 	render() {
 		return (
-			<div className="searchBar">
-				<div className="searchLeft">
-					<div className="instaLogo">
+			<SearchBarCSS>
+				<SearchLeft>
+					<InstaLogo>
 						<i className="fab fa-instagram" />
-					</div>
-					<div className="instaTitle">
-						<img src={instaTitle} alt="instagram" />
-					</div>
-				</div>
-				<input
-					className="searchInput"
+					</InstaLogo>
+					<InstaTitle>
+						<InstaImg src={instaTitle} alt="instagram" />
+					</InstaTitle>
+				</SearchLeft>
+				<SearchInput
 					placeholder="&#xF002; Search  "
 					onChange={this.props.searchChange}
 				/>
-				<div className="searchRight">
+				<SearchRight>
 					<div className="iconRight">
 						<i className="far fa-compass" />
 					</div>
@@ -39,10 +81,41 @@ class SearchBar extends React.Component {
 					<div onClick={this.logOut} className="iconRight">
 						<i className="far fa-user" />
 					</div>
-				</div>
-			</div>
+				</SearchRight>
+			</SearchBarCSS>
 		);
 	}
+
+	// render() {
+	// 	return (
+	// 		<div className="searchBar">
+	// 			<div className="searchLeft">
+	// 				<div className="instaLogo">
+	// 					<i className="fab fa-instagram" />
+	// 				</div>
+	// 				<div className="instaTitle">
+	// 					<img src={instaTitle} alt="instagram" />
+	// 				</div>
+	// 			</div>
+	// 			<input
+	// 				className="searchInput"
+	// 				placeholder="&#xF002; Search  "
+	// 				onChange={this.props.searchChange}
+	// 			/>
+	// 			<div className="searchRight">
+	// 				<div className="iconRight">
+	// 					<i className="far fa-compass" />
+	// 				</div>
+	// 				<div className="iconRight">
+	// 					<i className="far fa-heart" />
+	// 				</div>
+	// 				<div onClick={this.logOut} className="iconRight">
+	// 					<i className="far fa-user" />
+	// 				</div>
+	// 			</div>
+	// 		</div>
+	// 	);
+	// }
 }
 
 export default SearchBar;
